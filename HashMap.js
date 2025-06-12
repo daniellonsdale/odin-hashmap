679d8712571428e8cfd264d26c637df7f0bf60f7
daniellonsdale <danielllonsdale@gmail.com>
2025-06-13 00:39:36 +0900

    Set up init fields and constructor

--- HashMap.js
+++ HashMap.js
@@ -1,1 +1,11 @@
 import { LinkedList } from "./LinkedList.js";
+
+export class HashMap {
+    loadFactor;
+    capacity;
+    
+    constructor(loadFactor, capacity) {
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+    }
+}
