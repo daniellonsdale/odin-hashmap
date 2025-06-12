749e3d5375bbbdb005d3ca664c0d88ad2b0a4324
daniellonsdale <danielllonsdale@gmail.com>
2025-06-13 00:42:01 +0900

    Use TOP's hashing function with modulo to prevent integer overflow

--- HashMap.js
+++ HashMap.js
@@ -3,9 +3,21 @@ import { LinkedList } from "./LinkedList.js";
 export class HashMap {
     loadFactor;
     capacity;
-    
+
     constructor(loadFactor, capacity) {
         this.loadFactor = loadFactor;
         this.capacity = capacity;
+    }
+
+    hash(key){
+        let hashCode = 0;
+      
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = hashCode % this.capacity;
+        }
+
+        return hashCode;
     }
 }
